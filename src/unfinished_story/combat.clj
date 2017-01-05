1d002deb602c4ef7bdcd089cbe5f1068e2b07115
Alexey Kutishchev <okut.isd.dp.ua@gmail.com>
2017-01-05 14:00:49 -0800

    Switching to group combat

--- src/unfinished_story/combat.clj
+++ src/unfinished_story/combat.clj
@@ -2,15 +2,74 @@
   (:require [unfinished-story.creature :as creature])
   (:require [clojure.data.priority-map :as priority]))
 
-(declare map-win-lose bind hit-event hit dam-fun-supplier find-first)
+(declare map-win-lose bind hit-event own-group
+         hit dam-fun-supplier find-first
+         find-by-id target-group try-find-by find-by)
 
 
 ;; Auto battles with only attack actions.
-(defn auto-combat [init-state]
-    (let [actor (first (init-state :priority-map))])
+(defn auto-combat [state]
+    (let [actor-id (first (first (state :priority-map))) priority-queue (state :priority-map)]
+      (if (and (some? creature/alive? (state :attackers))
+               (some? creature/alive? (state :defenders)))
+
+          (let [actor (find-by-id actor-id) enemy-group (target-group state actor)]
+
+            (with-priority (dissoc priority-queue actor-id))
+
+              (update-in state [(own-group actor) (indices (fn [x] (= (x :id) actor-id))) (state own-group)]
+                       hit (first enemy-group)))
+
+      )))
+
+;; Retrieve information about creature: creature, group and index
+(defn lookup-actor [state actor-id]
+
   )
 
-;; Finds create by id in combat state.
+
+
+
+;;Updates state with new prioriy-map
+(defn with-priority [state new-priorities]
+  (assoc state :priority-map new-priorities))
+
+;;Updates state with creature updates.
+(defn with-creature [state creature]
+  (assoc state :priority-map creature))
+
+;; Return group opposite to grop in each current creature is.
+(defn target-group [creature state]
+  (if (try-find-by (state :attackers) :id (creature :id))
+    (state :defenders)
+           (state :attackers)))
+
+;; Return group opposite to grop in each current creature is.
+(defn own-group [creature state]
+  (if (try-find-by (state :attackers) :id (creature :id))
+    (state :attackers)
+           (state :defenders)))
+
+
+;; Find element index in vector
+(defn indices [pred coll]
+   (keep-indexed #(when (pred %2) %1) coll))
+
+;; Finds random target between creatures still alive in enemy group.
+(defn find-target [coll] (find-first creature/alive? coll))
+
+
+;; Finds create by id in combat state. Result can be nil.
+(defn try-find-by [coll field val]
+  (find-first (fn [x] (= (x field) val)) coll)
+  )
+
+;; Finds create by id in combat state. Result should be present.
+(defn find-by-id [coll id]
+  (find-by coll :id id)
+  )
+
+;; Finds create by id in combat state. Result should be present.
 (defn find-by [coll field val]
   {:post [(some? %)]}
   (find-first (fn [x] (= (x field) val)) coll)
@@ -34,7 +93,8 @@
   (let [p (priority/priority-map)]
     {:attackers attackers
      :defenders defenders
-     :priority-map (conj p (map (fn [c] [(c :id) (creature/initiative c)]) (concat attackers defenders)))}
+     :system-time 0
+     :priority-map (into p (into [] (map (fn [c] [(c :id) (creature/initiative c)]) (concat attackers defenders))))}
   ))
 
 
@@ -51,10 +111,12 @@
     (map-win-lose attacker defender queue)))
 
 ;;One creature hits another one for certain amount of damage.
-(defn hit[attacker, defender, damage-fun]
+(defn hit
+  ([attacker defender] (hit attacker defender (dam-fun-supplier)))
+  ([attacker, defender, damage-fun]
   (let [damage (damage-fun (get attacker :damage))]
     [(assoc defender :cur-hp
-      (- (get defender :cur-hp) damage)) (hit-event attacker defender damage)]))
+      (- (get defender :cur-hp) damage)) (hit-event attacker defender damage)])))
 
 ;; Collect winner/looser map
 (defn map-win-lose [first, second, queue]
